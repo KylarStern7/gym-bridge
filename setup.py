4b9dd34a2c8c54c8b4dfd13edb41e1fe31fe0e77
Michał Kocon <m.kocon@ksmvision.pl>
2020-12-10 22:05:24 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_bridge',
       version='0.0.1',
-      install_requires=['gym', 'numpy', 'pyglet']
+      install_requires=['gym', 'numpy', 'pyglet', 'tensorflow']
       )
